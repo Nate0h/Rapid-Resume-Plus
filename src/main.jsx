c3044c60d10d320e29534c3e4e8c620331b7dee5
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-02-10 23:50:45 -0500

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import PersonalDetails from "./components/personalDetails.jsx";
-import Education from "./components/education.jsx";
-import Resume from "./components/resume.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
